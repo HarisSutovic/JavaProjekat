293b45ebfe494609dc30f78b1b7e603e14f0ebd0
Shehidd <shehidsoldier@gmail.com>
2025-10-02 11:18:46 +0200

    Add files via upload

---
+++ GrupniProjekat/src/GrupniProjekat/Player.java
@@ -0,0 +1,32 @@
+package GrupniProjekat;
+
+public class Player {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		// Clanovi: Shehid Bisha 23/006, Haris Sutovic 24/084, Dalil Hadziahmetovic 24/043
+		
+		private int x;
+		private int y;
+		
+		private int width;
+		private int height;
+		
+		private int health;
+		
+		public Player(int x, int y, int width, int height, int health) {
+			this.x = x;
+			this.y = y;
+			this.width = width;
+			this.height = height;
+			setHealth(health); }
+		
+	
+		
+			
+			
+		}
+	}
+
+}
